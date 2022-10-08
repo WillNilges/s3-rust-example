cb6726687a95355e1d9b9740f918bf3e523432a3
Will Nilges <wdn5796@rit.edu>
2022-10-08 04:07:35 -0400

    aaaa too much scaffold

--- onboard/src/main.rs
+++ onboard/src/main.rs
@@ -1,11 +1,11 @@
 mod devcade_s3;
 use devcade_s3::S3;
 
-mod application_row;
-use crate::application_row::ApplicationRow;
+mod data_set;
+use std::str::FromStr;
 
 use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow, Button, gio};
+use gtk4::{Application, ApplicationWindow, Button, gio, glib, gdk};
 const APP_ID: &str = "edu.csh.rit.devcade.onboard";
 
 // GUI Functions
@@ -15,112 +15,48 @@ fn build_ui(app: &gtk4::Application) {
         .default_width(600)
         .default_height(600)
         .application(app)
-        .title("ListView: Applications Launcher")
+        .title("FlowBox")
         .build();
 
-    let model = gio::ListStore::new(gio::AppInfo::static_type());
-    gio::AppInfo::all().iter().for_each(|app_info| {
-        model.append(app_info);
-    });
+    let flow_box = gtk4::FlowBox::builder()
+        .valign(gtk4::Align::Start)
+        .max_children_per_line(30)
+        .min_children_per_line(4)
+        .selection_mode(gtk4::SelectionMode::None)
+        .build();
 
-    let factory = gtk4::SignalListItemFactory::new();
-    // the "setup" stage is used for creating the widgets
-    factory.connect_setup(move |_factory, item| {
-        // In gtk4 < 4.8, you don't need the following line
-        // as gtk used to pass GtkListItem directly. In order to make that API
-        // generic for potentially future new APIs, it was switched to taking a GObject in 4.8
-        let item = item.downcast_ref::<gtk4::ListItem>().unwrap();
-        let row = ApplicationRow::new();
-        item.set_child(Some(&row));
-    });
-
-    // the bind stage is used for "binding" the data to the created widgets on the "setup" stage
-    factory.connect_bind(move |_factory, item| {
-        // In gtk4 < 4.8, you don't need the following line
-        // as gtk used to pass GtkListItem directly. In order to make that API
-        // generic for potentially future new APIs, it was switched to taking a GObject in 4.8
-        let item = item.downcast_ref::<gtk4::ListItem>().unwrap();
-        let app_info = item.item().unwrap().downcast::<gio::AppInfo>().unwrap();
-
-        let child = item.child().unwrap().downcast::<ApplicationRow>().unwrap();
-        child.set_app_info(&app_info);
-    });
-
-    // A sorter used to sort AppInfo in the model by their name
-    let sorter = gtk4::CustomSorter::new(move |obj1, obj2| {
-        let app_info1 = obj1.downcast_ref::<gio::AppInfo>().unwrap();
-        let app_info2 = obj2.downcast_ref::<gio::AppInfo>().unwrap();
-
-        app_info1
-            .name()
-            .to_lowercase()
-            .cmp(&app_info2.name().to_lowercase())
-            .into()
-    });
-    let sorted_model = gtk4::SortListModel::new(Some(&model), Some(&sorter));
-    let selection_model = gtk4::SingleSelection::new(Some(&sorted_model));
-
-    let list_view = gtk4::ListView::new(Some(&selection_model), Some(&factory));
-    // Launch the application when an item of the list is activated
-    list_view.connect_activate(move |list_view, position| {
-        let model = list_view.model().unwrap();
-        let app_info = model
-            .item(position)
-            .unwrap()
-            .downcast::<gio::AppInfo>()
-            .unwrap();
-
-        let context = list_view.display().app_launch_context();
-        if let Err(err) = app_info.launch(&[], Some(&context)) {
-            let parent_window = list_view.root().unwrap().downcast::<gtk4::Window>().unwrap();
-
-            gtk4::MessageDialog::builder()
-                .text(&format!("Failed to start {}", app_info.name()))
-                .secondary_text(&err.to_string())
-                .message_type(gtk4::MessageType::Error)
-                .modal(true)
-                .transient_for(&parent_window)
-                .build()
-                .show();
-        }
+    data_set::COLORS.iter().for_each(|color| {
+        let color_widget = create_color_button(color);
+        flow_box.insert(&color_widget, -1);
     });
 
     let scrolled_window = gtk4::ScrolledWindow::builder()
         .hscrollbar_policy(gtk4::PolicyType::Never) // Disable horizontal scrolling
         .min_content_width(360)
-        .child(&list_view)
+        .child(&flow_box)
         .build();
 
     window.set_child(Some(&scrolled_window));
     window.show();
 }
-/*
-fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
+
+fn create_color_button(color: &'static str) -> gtk4::Button {
+    let button = gtk4::Button::new();
+    let drawing_area = gtk4::DrawingArea::builder()
+        .content_height(24)
+        .content_width(24)
         .build();
 
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
+    let rgba = gdk::RGBA::from_str(color).unwrap();
+    drawing_area.set_draw_func(move |_, cr, _width, _height| {
+        GdkCairoContextExt::set_source_rgba(cr, &rgba);
+        cr.paint().expect("Invalid cairo surface state");
     });
+    button.set_child(Some(&drawing_area));
 
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .child(&button)
-        .build();
+    button
+}
 
-    // Present window
-    window.present();
-}*/
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -134,7 +70,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     s3_conn.show_buckets().await?;
     s3_conn.show_objects("devcade-games").await?;
 
-    let game_uri = s3_conn.get_object(&bucket, &object, 900).await?;
+    let game_uri = s3_conn.get_object(&bucket, &object, 90).await?; // TODO (willnilges): if this
+                                                                    // doesn't work, I reduced the
+                                                                    // expiry time. Try increasing it.
 
     // Download game, create directory if we need to.
 //    std::fs::create_dir(devcade_games_dir)?;
